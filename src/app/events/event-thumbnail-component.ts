bbce8df692d44e32a3950a47dd48f52a09455b41
akshay <engaksiit@gmail.com>
2019-01-18 17:40:39 +0530

    Adding style using ngClass with funtion

--- src/app/events/event-thumbnail-component.ts
+++ src/app/events/event-thumbnail-component.ts
@@ -7,7 +7,7 @@ import { Output } from '@angular/core';
     <div class = "well hoverwell thumbnail">
         <h2>{{event?.name}}</h2>
         <div> Date : {{event?.date}} </div>
-        <div [ngClass]="{green:event?.time === '10:00 am', bold:event?.time==='10:00 am'}" [ngSwitch]="event?.time"> Time: {{event?.time}}
+        <div [ngClass]="getStyleClasses()" [ngSwitch]="event?.time"> Time: {{event?.time}}
         <span *ngSwitchCase="'08:00 am'">(Early start)</span>
         <span *ngSwitchCase="'10:00 am'">(Normal Start)</span>
         <span *ngSwitchDefault> (LateStart)</span>
@@ -41,4 +41,10 @@ export class EventThumbNailComponent{
 // {
 //     console.log("foo");
 // }
+
+    getStyleClasses()
+    {
+        let isNormalStart = this.event.time === '10:00 am';
+        return {green:isNormalStart, bold:isNormalStart}
+    }
 }
