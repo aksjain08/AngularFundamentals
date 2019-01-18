058bca9b320fadccdde0ef622d610708d91a76cf
akshay <engaksiit@gmail.com>
2019-01-18 17:19:20 +0530

    Ng Swith Case Statement inline

--- src/app/events/event-thumbnail-component.ts
+++ src/app/events/event-thumbnail-component.ts
@@ -7,13 +7,12 @@ import { Output } from '@angular/core';
     <div class = "well hoverwell thumbnail">
         <h2>{{event?.name}}</h2>
         <div> Date : {{event?.date}} </div>
-        <div> Time: {{event?.time}}</div>
-        <div> Price : \${{event.price}} </div>
-        <div [ngSwitch]="event?.time">
-            <span *ngSwitchCase="'08:00 am'">Early start</span>
-            <span *ngSwitchCase="'10:00 am'">Mormal Start</span>
-            <span *ngSwitchDefault> LateStart</span>
+        <div [ngSwitch]="event?.time"> Time: {{event?.time}}
+        <span *ngSwitchCase="'08:00 am'">(Early start)</span>
+        <span *ngSwitchCase="'10:00 am'">(Normal Start)</span>
+        <span *ngSwitchDefault> (LateStart)</span>
         </div>
+        <div> Price : \${{event.price}} </div>
         <div *ngIf="event?.location">
             <span> Location : {{event.location.address}}</span>
         </div>
